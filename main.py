24f48fbb2116727b0133e6a2477be1770f51d881
zzzhrq <Zach0020@e.ntu.edu.sg>
2024-09-13 05:32:43 +0000

    fix: logic flaw

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 import streamlit as st
 
 conversion = {
-    "HEX": 16,
-    "DEC": 10,
-    "OCT": 8,
-    "BIN": 2
+	"HEX": 16,
+	"DEC": 10,
+	"OCT": 8,
+	"BIN": 2
 }
 
 st.title("Number Converter")
 number = st.text_input("Enter a number")
-conversion_start = st.selectbox("Select conversion type", list(conversion.keys()))
+conversion_start = st.selectbox("Select the base of the input number", list(conversion.keys()))
 
-num_int = int(number, conversion[conversion_start])
-conversion_type = st.selectbox("Select conversion type", list(conversion.keys()))
+if number:
+	try:
+		# Convert the input number to an integer based on the selected base
+		num_int = int(number, conversion[conversion_start])
+		conversion_type = st.selectbox("Select the target base for conversion", list(conversion.keys()))
 
-converted_number = None
+		# Convert the integer to the selected base
+		if conversion_type == "BIN":
+			converted_number = bin(num_int)[2:]  # Remove the '0b' prefix
+		elif conversion_type == "HEX":
+			converted_number = hex(num_int)[2:]  # Remove the '0x' prefix
+		elif conversion_type == "OCT":
+			converted_number = oct(num_int)[2:]  # Remove the '0o' prefix
+		else:
+			converted_number = str(num_int)
 
-# Convert the number to the selected base
-if conversion_type == "BIN":
-    converted_number = bin(num_int)[2:]  # Remove the '0b' prefix
-elif conversion_type == "HEX":
-    converted_number = hex(num_int)[2:]  # Remove the '0x' prefix
-elif conversion_type == "OCT":
-    converted_number = oct(num_int)[2:]  # Remove the '0o' prefix
+		st.write(f"Conversion of {number} from {conversion_start} to {conversion_type} is {converted_number}")
+	except ValueError:
+		st.write("Invalid number input")
 else:
-    converted_number = str(num_int)
-
-st.write(f"Conversion of {number} to {conversion_type} is {converted_number}")
-
+	st.write("Please enter a number")

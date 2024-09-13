0e01e906c80f1b58609abe491de5e861b9265451
zzzhrq <Zach0020@e.ntu.edu.sg>
2024-09-13 05:16:37 +0000

    fix: logic flaw

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ streamlit.title("Number Converter")
 number = streamlit.number_input("Enter a number", value=0, step=1)
 conversion_type = streamlit.selectbox("Select conversion type", list(conversion.keys()))
 
-streamlit.write(f"Conversion of {number} to {conversion_type} is {int(number):{conversion[conversion_type]}}")
+streamlit.write(f"Conversion of {number} to {conversion_type} is {int(number):{int(number,conversion[conversion_type])}}")
 
